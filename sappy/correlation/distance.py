f050605adb85a2eb2312c09ba3b7ed604f45f475
hrk0531 <hrk0531.dev@gmail.com>
2024-07-13 06:07:32 +0900

    Fix me

--- sappy/correlation/distance.py
+++ sappy/correlation/distance.py
@@ -8,6 +8,12 @@ import numpy as np
 def distance(data: np.ndarray[Any, Any], method: str = "euclidean") -> np.ndarray[Any, Any]:
     """
     Compute distance matrix for given method.
+
+    Args:
+        data (np.ndarray): Dataset where rows are subjects and columns are variables.
+
+    Returns:
+        np.ndarray: Distance matrix.
     """
     x = data.copy()
     n_rows = x.shape[0]
@@ -20,8 +26,26 @@ def distance(data: np.ndarray[Any, Any], method: str = "euclidean") -> np.ndarra
 
     return dist_mtx
 
-def mds(dist_mtx: np.ndarray[Any, Any], dimension: int, dissimilarity="precomputed"):
+
+def mds(dist_mtx: np.ndarray[Any, Any], n_components: int) -> np.ndarray[Any, Any]:
     """
     """
+    d = dist_mtx.copy()
+
     # The size of distance matrix
-    n_rows = dist_mtx.shape[0]
+    n_rows, n_cols = dist_mtx.shape
+
+    # H is the centering matrix
+    h = np.eye(n_rows) - np.ones((n_rows, n_rows)) / n_cols
+
+    # Initial karnel matrix
+    k = -0.5 * h.dot(d**2).dot(h)
+
+    eigvals, eigvecs = np.linalg.eigh(k)
+    idx = eigvals.argsort()[::-1]
+    eigvals_idx = eigvals[idx]
+    eigvecs_idx = eigvecs[:, idx]
+
+    x = eigvecs_idx[:, :n_components].T @ np.diag(np.sqrt(eigvals_idx[:n_components]))
+
+    return x
